b12055cdadbb8595e7c921a6f794404fe895a0a6
kzzzzmi <jaemin21685@gmail.com>
2021-09-03 00:32:33 +0900

    LinkedList 2

--- algorithm/src/linkedList/MainTest1.java
+++ algorithm/src/linkedList/MainTest1.java
@@ -4,12 +4,22 @@ public class MainTest1 {
 
 	public static void main(String[] args) {
 
-		SingleLinkedList<Integer> myLinkedList = new SingleLinkedList<Integer>();
-		myLinkedList.addNode(1);
-		myLinkedList.addNode(2);
-		myLinkedList.addNode(3);
+		SingleLinkedList1<Integer> myList = new SingleLinkedList1<>();
+		myList.addNode(1);
+		myList.addNode(2);
+		myList.addNode(3);
 		
-		myLinkedList.printAll();
+		myList.addNode(5, 1);
+		myList.addNode(6, 3);
+		myList.addNode(7, 20);
+		myList.printAll();
+		
+		System.out.println("------------------------------");
+		
+		myList.deleteNode(20);
+		myList.printAll();
+		
+		
 	}
 
 }
