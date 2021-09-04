0b577af25c03542a00f9dd82315a30602e2ef013
kzzzzmi <jaemin21685@gmail.com>
2021-09-04 23:34:26 +0900

    studyinng

--- algorithm/src/aaaa/Main.java
+++ algorithm/src/aaaa/Main.java
@@ -6,14 +6,7 @@ import java.util.LinkedList;
 public class Main {
 
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		while(sc.hasNextInt()) {
-			int n1 = sc.nextInt();
-			int n2 = sc.nextInt();
-			System.out.println(n1 + n2);			
-		}
+		System.out.print("Hello World!");
 	}
 }
 
