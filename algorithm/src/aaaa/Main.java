84e0ecad6f8ad2c94838ded682fea48633d77661
kzzzzmi <jaemin21685@gmail.com>
2021-09-05 02:51:48 +0900

    algoritm

--- algorithm/src/aaaa/Main.java
+++ algorithm/src/aaaa/Main.java
@@ -1,12 +1,35 @@
 package aaaa;
 
 import java.util.Scanner;
-import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+
+		int cnt = 0;
+
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == 'c') {
+				if (((i + 1) < input.length()) && (input.charAt(i + 1) == '=' || input.charAt(i + 1) == '-')) {
+					i++;
+				}
+			} else if (input.charAt(i) == 'd') {
+				if (((i + 2) < input.length()) && (input.charAt(i + 1) == 'z' && input.charAt(i + 2) == '=')) {
+					i += 2;
+				} else if (((i + 1) < input.length()) && (input.charAt(i + 1) == '-')) {
+					i++;
+				}
+			} else if ((i + 1) < input.length() && (input.charAt(i) == 'l' && input.charAt(i + 1) == 'j'
+					|| input.charAt(i) == 'n' && input.charAt(i + 1) == 'j'
+					|| input.charAt(i) == 's' && input.charAt(i + 1) == '='
+					|| input.charAt(i) == 'z' && input.charAt(i + 1) == '=')) {
+				i++;
+			}
+			cnt++;
+		}
+		System.out.println(cnt);
 	}
 }
-
