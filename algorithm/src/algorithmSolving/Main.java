1b13c0bbe45f6d31dda9b5895865d70bc8c3dfa8
kzzzzmi <jaemin21685@gmail.com>
2021-09-09 02:00:17 +0900

    algorithm

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -3,24 +3,21 @@ package algorithmSolving;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int test = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		StringTokenizer st;
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
 		
-		for(int i=0; i<test; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int k = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
-			
-			System.out.println(k);
-			System.out.println(n);
-		}
+		A = A.add(B);
+		System.out.println(A);
 		
 	}
 }
